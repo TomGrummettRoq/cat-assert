699c4f572af8af9dcd733166e570a28092021482
Tom Grummmett <tom.grummett@roq.co.uk>
2023-02-21 15:03:52 +0000

    Initial positive Tests

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -6,10 +6,50 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class CatTest {
+	
 	@Test
 	public void testInitialState() {
 		Cat cat = new Cat();
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+
+	@Test
+	public void testFeedCat() {
+		Cat cat = new Cat(); {
+			cat.feeds();
+		};
+		assertEquals(cat.howHungry(), 30);
+	}
+	
+	@Test
+	public void testRunCat() {
+		Cat cat = new Cat(); {
+			cat.runs();
+		};
+		assertEquals(cat.howHungry(), 80);
+	}
+		
+	@Test
+	public void testRunFeedRun() {
+		Cat cat = new Cat(); {
+			cat.runs();
+			cat.feeds();
+			cat.runs();
+		};
+		assertEquals(cat.howHungry(), 90);
+	}
+	
+	@Test
+	public void testLowestLevel() {
+		Cat cat = new Cat(); {
+			cat.feeds();
+			cat.feeds();
+			cat.feeds();
+		};
+		assertTrue(cat.isAlive());
+		assertEquals(cat.howHungry(), 0);
+	}
+	
 }
+
